918c5960a7daadcf198563b0957eec4cb3f3272f
HyFredInMinecraft <tobylechelt@gmail.com>
2025-11-25 17:28:45 -0700

    Create Toby: sketch.js

---
+++ Toby: sketch.js
@@ -0,0 +1,45 @@
+let PlayerBImage;
+let PlayerBY = 50;
+let PlayerBX = 67; // idk bro
+let PlayerRImage;
+let PlayerRY = 50;
+let PlayerRX = 333;
+let speed = 3;
+let timer;
+var VW = 400;
+var VH = 600;
+let OnGroundB = false;
+let OnGroundR = false;
+let GravityB = 1.5;
+let MotionB = 0;
+let GravityR = 1.5;
+let MotionR = 0;
+
+if (VW >= VH){
+var REM = VH/160;
+}else{
+var REM = VW/160;
+}
+
+let Upscale = 1
+VW = VW * Upscale;
+VH = VH * Upscale;
+REM = REM * Upscale;
+
+function preload(){
+PlayerBImage = loadImage("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAA0klEQVR4nKSTLQ4CMRCFZzfr0Q0JQYHrNeowOO6ABY0EyxUIDoOr5BigCAmp5gQlbZhmOv2BsE9tZvZ7nd157SCVhe9qkgeEtdYgpazSQojANhx2Wu5OoTgYTiP4vF1EJi13r8HjyQhm62NU66AgDjvdb4+kVjT4Vb0N2lLj9bzCXwb71bwK4BY+smjgw6OUikz4FHSFuHL3D6LwYPNy2GQnoDAaJMJJ6ItOeAj2IRckBOlUmWg3wKJsKcxP5J8AtcuEJmRML2MMwlWDYML6uZrXGwAA///rE/i5AAAABklEQVQDAPT/RLRZ5yPnAAAAAElFTkSuQmCC");
+PlayerRImage = loadImage("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAwklEQVR4nKSTwQ3DIAxFTZQRcuHYBViDa+foEB2jc/TKND1yyQ40UEzA2DRKnhQpMvrfxjYz9AT4j+p+UOycA2PMUK21LlpFxZG3tSW4EPHD+8Zkou4j8W37Xj9hYQaBhYl9mJhocJTLBpN0sMJJg3uehAROIRPQIC2PzRNAk1UQxxHiyGMPmuXBw2ebqVCL0aADK3HkOpjE7rui2CZGYV0Vs9oKyCqHWkwz0ivA6DGhSVVmwm890fsaiwbFhJxzscQXAAD//8gwZTUAAAAGSURBVAMAz0FAI7sKTL4AAAAASUVORK5CYII=");
+}
+
+function setup() {
+  createCanvas(VW, VH);
+  textSize(18);
+}
+
+function draw() {
+  background(0,155,155);
+  fill(255,0,0);
+  //square(155,boxY,20);
+  PlayerB()
+  PlayerR()
+}
