7db448094d0f41fc761a597a45f520e7ff4e2dbd
HyFredInMinecraft <tobylechelt@gmail.com>
2025-11-27 19:42:39 -0700

    Update Toby: PlayerR.js

--- Toby: PlayerR.js
+++ Toby: PlayerR.js
@@ -8,13 +8,13 @@ function PlayerR()
   image(PlayerRImage,PlayerRX,PlayerRY,16*REM,16*REM);
   PlayerRY += GravityR*REM
   PlayerRX += MotionR*REM
-  if(PlayerRY < VH-16*REM)
+  if(PlayerRY < Bottom-16*REM)
   {
     OnGroundR = false
   }
-  if(PlayerRY > VH-16*REM)
+  if(PlayerRY > Bottom-16*REM)
   {
-    PlayerRY = VH-16*REM
+    PlayerRY = Bottom-16*REM
     OnGroundR = true
   }
   if(PlayerRX < 0*REM)
